316e33b79dfc7724ed3cf05608a04dcb8ff5656f
Coding-Jarl <senvisage@gmail.com>
2023-07-26 23:02:55 +0200

    feat: create Description component

---
+++ src/components/Description.tsx
@@ -0,0 +1,15 @@
+import styles from '@/styles/components/Description.module.scss'
+
+export default function Description() {
+  return (
+    <section className={styles.wrapper}>
+      <h2>Description</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea dolorum
+        dignissimos explicabo excepturi dicta maiores ipsa cum modi nostrum
+        repudiandae praesentium quasi delectus molestiae architecto esse eveniet
+        aliquid, omnis odio.
+      </p>
+    </section>
+  )
+}
